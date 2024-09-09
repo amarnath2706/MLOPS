99683dcd6e39bbac324783e324bf4f5feed783ff
Amar <amarnath.arjsd@gmail.com>
2024-09-09 19:07:00 +0530

    commit

---
+++ template.py
@@ -0,0 +1,17 @@
+import os 
+from pathlib import Path
+
+list_of_files = [
+
+    ".github/workflows",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_training.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+
+]
