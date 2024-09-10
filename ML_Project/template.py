2e9092ab788b65a4040cc484371b6f4c83be1e4e
Amar <amarnath.arjsd@gmail.com>
2024-09-10 11:06:55 +0530

    commit

--- ML_Project/template.py
+++ ML_Project/template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 list_of_files = [
 
-    ".github/workflows",
+    ".github/workflows/.gitkeep",
     "src/__init__.py",
     "src/components/__init__.py",
     "src/components/data_ingestion.py",
@@ -13,6 +13,7 @@ list_of_files = [
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception.py",
